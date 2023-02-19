bd231165930b848fb3b44ed80136e710d81298a8
Leonardo Malave <leomalave1993@gmail.com>
2023-02-19 16:11:28 -0400

    interactive-calculator

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import './App.css';
 import freeCodeCampLogo from './imagenes/freeCodeCamp-logo.png';
+import Boton from './componentes/Boton';
+import Pantalla from './componentes/Pantalla';
+import BotonClear from './componentes/BotonClear';
 
 function App() {
   return (
@@ -8,7 +11,37 @@ function App() {
         <img 
           src={freeCodeCampLogo} 
           className='freecodecamp-logo'
-          alt='Logo de freeCodeCamp' />
+          alt='Logo de freeCodeCamp' />         
+      </div>
+      <div className='contenedor-calculadora'>
+        <Pantalla />
+        <div className='fila'>
+          <Boton>1</Boton>
+          <Boton>2</Boton>
+          <Boton>3</Boton>
+          <Boton>+</Boton>
+        </div>
+        <div className='fila'>
+          <Boton>4</Boton>
+          <Boton>5</Boton>
+          <Boton>6</Boton>
+          <Boton>-</Boton>
+        </div>  
+        <div className='fila'>
+          <Boton>7</Boton>
+          <Boton>8</Boton>
+          <Boton>9</Boton>
+          <Boton>*</Boton>
+        </div>  
+        <div className='fila'>
+          <Boton>=</Boton>
+          <Boton>0</Boton>
+          <Boton>.</Boton>
+          <Boton>/</Boton>
+        </div>  
+        <div className='fila'>
+          <BotonClear>Clear</BotonClear>
+        </div>
       </div>
     </div>
   );
